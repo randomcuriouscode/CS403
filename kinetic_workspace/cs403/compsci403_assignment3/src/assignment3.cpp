f95a12b87a3cae7fa57237a0393d5fccdacb95e1
Tony Gao <zijian1994@gmail.com>
2018-02-14 17:16:18 -0800

    working on 3.4

--- kinetic_workspace/cs403/compsci403_assignment3/src/assignment3.cpp
+++ kinetic_workspace/cs403/compsci403_assignment3/src/assignment3.cpp
@@ -25,7 +25,11 @@ using geometry_msgs::Point32;
 
 ros::ServiceServer g_TransformPointSrv; // /COMPSCI403/TransformPoint
 ros::ServiceServer g_FitMinimalPlaneSrv; // /COMPSCI403/FitMinimalPlane
-ros::ServiceServer g_FindInliersSrv; // //COMPSCI403/FindInliers
+ros::ServiceServer g_FindInliersSrv; // /COMPSCI403/FindInliers
+ros::ServiceServer g_FitBestPlaneSrv; // /
+
+const float ESTIMATED_FIT_POINTS = .75f;
+const size_t RANSAC_MAX_ITER = 40;
 
 // Define service and callback functions
 
@@ -66,12 +70,13 @@ bool FitMinimalPlaneCallback(compsci403_assignment3::FitMinimalPlaneSrv::Request
 {
 	Vector3f v1 (req.P2.x - req.P1.x, 
 				req.P2.y - req.P1.y,
-				req.P2.z - req.p1.z ); //Vector P1P2
+				req.P2.z - req.P1.z ); //Vector P1P2
 	Vector3f v2 (req.P3.x - req.P1.x,
 				req.P3.y - req.P1.y,
 				req.P3.z - req.P1.z);
 
 	Vector3f n = v1.cross(v2); // calc normal
+	n = n / n.norm(); // normalize normal
 
 	Point32 p_n;
 
@@ -82,6 +87,59 @@ bool FitMinimalPlaneCallback(compsci403_assignment3::FitMinimalPlaneSrv::Request
 	res.n = p_n;
 	res.P0 = req.P1; // any point on plane can be P0
 
+	return true;
+}
+
+bool FindInliersCallback(compsci403_assignment3::FindInliersSrv::Request &req,
+						compsci403_assignment3::FindInliersSrv::Response &res)
+{
+	Vector3f n (req.n.x, req.n.y, req.n.z);
+
+	for (std::vector<Point32>::iterator it = req.P.begin(); 
+		 it != req.P.end(); it++)
+	{
+		Vector3f M  (it->x - req.P0.x,
+					 it->y - req.P0.y,
+					 it->z - req.P0.z); // M = (P - P0)
+
+		float d = M.dot(n);  // calculate distance
+
+		if (d <= req.epsilon)
+		{   // add to response inlier points list
+			res.P.push_back(*it); 
+		}
+	} 
+
+	return true;
+}
+
+/*
+  Helper to randomly select an item from a STL container, from stackoverflow.
+*/
+template <typename I>
+I random_element(I begin, I end)
+{
+    const unsigned long n = std::distance(begin, end);
+    const unsigned long divisor = (RAND_MAX + 1) / n;
+
+    unsigned long k;
+    do { k = std::rand() / divisor; } while (k >= n);
+
+    std::advance(begin, k);
+    return begin;
+}
+
+bool FitBestPlaneCallback(compsci403_assignment3::FitBestPlaneSrv::Request &req,
+						  compsci403_assignment3::FitBestPlaneSrv::Response &res)
+{
+	std::vector<Point32> all_points = req.P;
+	for (int iterations = 0; iterations < RANSAC_MAX_ITER; iterations ++)
+	{
+		// get 3 random points 
+		Point32 p1 = *random_element(all_points.begin(), all_points.end());
+		Point32 p2 = *random_element(all_points.begin(), all_points.end());
+		Point32 p3 = *random_element(all_points.begin(), all_points.end());
+	}
 	return true;
 }
 
@@ -96,6 +154,12 @@ int main(int argc, char **argv) {
 
   //2. Provide service FitMinimalPlane
   g_FitMinimalPlaneSrv = n.advertiseService("/COMPSCI403/FitMinimalPlane", FitMinimalPlaneCallback);
+
+  //3. Provide Service FindInliers
+  g_FindInliersSrv = n.advertiseService("/COMPSCI403/FindInliers", FindInliersCallback);
+
+  //4. Provide /COMPSCI403/FitBestPlane
+  g_FitBestPlaneSrv = n.advertiseService("/COMPSCI403/FitBestPlane", FitBestPlaneCallback);
 
   ros::spin();
 
