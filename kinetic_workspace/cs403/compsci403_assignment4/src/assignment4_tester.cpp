97cf8f09052db1ed35fba4c73553a7cdd63fe17f
Tony Gao <zijian1994@gmail.com>
2018-02-25 17:59:54 -0800

    adding assignment4 tester node

---
+++ kinetic_workspace/cs403/compsci403_assignment4/src/assignment4_tester.cpp
@@ -0,0 +1,29 @@
+#include <ros/ros.h>
+#include <ros/package.h>
+#include <geometry_msgs/Point32.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <sensor_msgs/LaserScan.h>
+
+#include "cobot_msgs/CobotDriveMsg.h"
+#include "compsci403_assignment4/ObstacleMsg.h"
+#include "compsci403_assignment4/CheckPointSrv.h"
+#include "compsci403_assignment4/GetFreePathSrv.h"
+#include "compsci403_assignment4/GetCommandVelSrv.h"
+
+// Include any additional header or service/message files
+
+// Declare class variables, subscribers, publishers, messages
+
+// Define service and callback functions
+
+int main(int argc, char **argv) {
+
+  ros::init(argc, argv, "assignment4_tester");
+  ros::NodeHandle n;
+
+	// Perform operations defined in Assignment 4
+
+	ros::spin();
+
+  return(0);
+}
