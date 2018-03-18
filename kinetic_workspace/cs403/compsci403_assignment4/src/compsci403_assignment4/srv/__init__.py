71e997b6b77df0a8f19392c14cf5d595d8c25327
rpg711 <zijian1994@gmail.com>
2018-03-17 22:18:12 -0700

    adding assignment5 assets

--- kinetic_workspace/cs403/compsci403_assignment4/src/compsci403_assignment4/srv/__init__.py
+++
@@ -1,3 +0,0 @@
-from ._CheckPointSrv import *
-from ._GetCommandVelSrv import *
-from ._GetFreePathSrv import *
---
+++ kinetic_workspace/cs403/compsci403_assignment5/src/assignment5.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <iostream>
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+#include <sstream>
+#include <math.h>
+
+#include <eigen3/Eigen/Dense>
+#include <geometry_msgs/Point32.h>
+#include <geometry_msgs/Point.h>
+#include <sensor_msgs/LaserScan.h>
+#include <sensor_msgs/PointCloud.h>
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
+#include <visualization_msgs/Marker.h>
+
+#include "compsci403_assignment5/CheckPointSrv.h"
+#include "compsci403_assignment5/GetFreePathSrv.h"
+#include "compsci403_assignment5/ObstacleLaserScanSrv.h"
+#include "compsci403_assignment5/GetCommandVelSrv.h"
+#include "compsci403_assignment5/GetTransformationSrv.h"
+
+using Eigen::Matrix3f;
+using Eigen::MatrixXf;
+using Eigen::MatrixXd;
+using Eigen::Vector3f;
+using Eigen::Vector2f;
+using geometry_msgs::Point32;
+using geometry_msgs::Point;
+using geometry_msgs::Twist;
+using nav_msgs::Odometry;
+using visualization_msgs::Marker;
+using sensor_msgs::LaserScan;
+using sensor_msgs::PointCloud;
+using std::cout;
+using std::vector;
+using namespace std;
+
+// Global Parameters
+const float gRobotRadius = 0.18;
+const float gRobotHeight = 0.36;
+const float gEpsilon = 0.15;
+const float gAMaxLin = 1 * 0.5; // m/s^2
+const float gAMaxRot = 1 * 2.0; // rad/s^2
+const float gVMaxLin = 0.5; // m/s
+const float gVMaxRot = 1.5; // rad/s
+const float gDT = 0.02; // s
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "compsci403_assignment5");
+  ros::NodeHandle n;
+
+  ros::spin();
+
+  return 0;
+}
